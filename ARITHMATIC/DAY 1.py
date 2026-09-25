aead59389d5190a4c25f33e825c121a9f2f5a0bb
yazhinisamy2007-debug <yazhinisamy2007@gmail.com>
2026-09-25 14:35:10 +0530

    ARITHMETIC

---
+++ ARITHMATIC/DAY 1.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Modulus =", a % b)
