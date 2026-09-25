a904f9755d3935727b165cae5793dfae4101b82a
yazhinisamy2007-debug <yazhinisamy2007@gmail.com>
2026-09-25 14:07:51 +0530

    Add files via upload

---
+++ mark/DAY-03.py
@@ -0,0 +1,8 @@
+java=float(input("enter a value:"))
+python=float(input("enter a value:"))
+mysql=float(input("enter a value:"))
+print("total:",java+python+mysql)
+print("average:",java+python+mysql/3)
+print("percentage:",java+python+mysql/3*100)
+print("remaining mark:",300-java+python+mysql)
+
