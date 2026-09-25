5094c919ef59766bd0bbc30d7fbdbbc69d52ffbb
yazhinisamy2007-debug <yazhinisamy2007@gmail.com>
2026-09-25 13:49:30 +0530

    DAY02-AREA

---
+++ area/DAY02-AREA.py
@@ -0,0 +1,48 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+===== RESTART: C:/Users/student120/Desktop/New folder/MY PROJECT/DAY-02.py =====
+
+--- Area Calculator ---
+1. Circle
+2. Rectangle
+3. Square
+4. Exit
+Enter your choice: 8
+Invalid choice. Please enter 1, 2, 3, or 4.
+
+--- Area Calculator ---
+1. Circle
+2. Rectangle
+3. Square
+4. Exit
+Enter your choice: 1
+Enter radius: 3.4 
+Area of Circle = 28
+
+--- Area Calculator ---
+1. Circle
+2. Rectangle
+3. Square
+4. Exit
+Enter your choice: 2
+Enter length: 4.5
+Enter width: 5
+Area of Rectangle = 20
+
+--- Area Calculator ---
+1. Circle
+2. Rectangle
+3. Square
+4. Exit
+Enter your choice: 3
+Enter side: 45
+Area of Square = 2025
+
+--- Area Calculator ---
+1. Circle
+2. Rectangle
+3. Square
+4. Exit
+Enter your choice: 4
+Program exited.
